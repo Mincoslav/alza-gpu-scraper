06bcfe6c6afbd4af8aea6a4716c861ef86b109c1
Michal <mincoslav@gmail.com>
2021-02-15 21:38:02 +0100

    Initial commit

---
+++ alza_gpu_scraper.py
@@ -0,0 +1,61 @@
+import time
+from tkinter import messagebox
+
+import selenium.webdriver
+import yagmail
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = selenium.webdriver.Chrome(options=chrome_options)
+
+
+def send_email(gpu_link: str):
+    receiver = "mincoslav@gmail.com"
+    body = gpu_link
+
+    yag = yagmail.SMTP()
+    yag.send(
+        to=receiver,
+        subject="Wake the fuck up Samurai, there's a GPU to scalp",
+        contents=body,
+    )
+
+
+while True:
+    driver.get('https://www.alza.sk/graficke-karty-nvidia-geforce-rtx-3060-ti/18884150.htm')
+    xpath = '//*[@data-impression-category="Komponenty \ Grafické karty \ NVIDIA"]'
+    main_page = EC.visibility_of_element_located((By.XPATH, xpath))
+    WebDriverWait(driver, 20).until(main_page)
+
+    cards = driver.find_elements_by_xpath(xpath)
+    del cards[::2]
+
+    for card in cards:
+
+        if "Na sklade" in card.get_attribute('data-impression-dimension13'):
+            messagebox.showinfo("Na sklade", '\n-------------------------------'
+                                + card.get_attribute('data-impression-name')
+                                + "\n" + card.get_attribute('href') + "\n"
+                                + card.get_attribute('data-impression-dimension13') + '✔')
+
+            send_email("Na sklade" + '\n-------------------------------'
+                       + card.get_attribute('data-impression-name')
+                       + "\n" + card.get_attribute('href') + "\n"
+                       + card.get_attribute('data-impression-dimension13') + '✔')
+
+        elif "Očakávame" in card.get_attribute('data-impression-dimension13'):
+            print(card.get_attribute('data-impression-name') + " -> "
+                  + card.get_attribute('data-impression-dimension13') + " ❔")
+
+        else:
+            print(card.get_attribute('data-impression-name') + " -> "
+                  + card.get_attribute('data-impression-dimension13') + " ❌")
+
+    time.sleep(5)
+    driver.refresh()
