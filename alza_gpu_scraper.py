fd46e11939d00b0980062b22d34669c55c3d0a2b
Michal Martincek <michal.martincek@nexthink.com>
2021-06-15 17:47:50 +0200

    chromedriver update added script for risograph checking updated email sending

--- alza_gpu_scraper.py
+++ alza_gpu_scraper.py
@@ -15,47 +15,51 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 driver = selenium.webdriver.Chrome(options=chrome_options)
 
 
-def send_email(gpu_link: str):
-    receiver = ""
-    body = gpu_link
+def send_email(contents: str, subject: str, receiver: str, user: str, password: str):
 
-    yag = yagmail.SMTP()
+    yag = yagmail.SMTP(user, password)
     yag.send(
         to=receiver,
-        subject="Wake the fuck up Samurai, there's a GPU to scalp",
-        contents=body,
+        subject=subject,
+        contents=contents,
     )
 
 
-while True:
-    driver.get('https://www.alza.sk/graficke-karty-nvidia-geforce-rtx-3060-ti/18884150.htm')
-    xpath = '//*[@data-impression-category="Komponenty \ Grafické karty \ NVIDIA"]'
-    main_page = EC.visibility_of_element_located((By.XPATH, xpath))
-    WebDriverWait(driver, 20).until(main_page)
+def main():
+    while True:
+        driver.get('https://www.alza.sk/graficke-karty-nvidia-geforce-rtx-3060-ti/18884150.htm')
+        xpath = '//*[@data-impression-category="Komponenty \ Grafické karty \ NVIDIA"]'
+        main_page = EC.visibility_of_element_located((By.XPATH, xpath))
+        WebDriverWait(driver, 20).until(main_page)
 
-    cards = driver.find_elements_by_xpath(xpath)
-    del cards[::2]
+        cards = driver.find_elements_by_xpath(xpath)
+        del cards[::2]
 
-    for card in cards:
+        for card in cards:
 
-        if "Na sklade" in card.get_attribute('data-impression-dimension13'):
-            messagebox.showinfo("Na sklade", '\n-------------------------------'
-                                + card.get_attribute('data-impression-name')
-                                + "\n" + card.get_attribute('href') + "\n"
-                                + card.get_attribute('data-impression-dimension13') + '✔')
+            if "Na sklade" in card.get_attribute('data-impression-dimension13'):
+                messagebox.showinfo("Na sklade", '\n-------------------------------'
+                                    + card.get_attribute('data-impression-name')
+                                    + "\n" + card.get_attribute('href') + "\n"
+                                    + card.get_attribute('data-impression-dimension13') + '✔')
 
-            send_email("Na sklade" + '\n-------------------------------'
-                       + card.get_attribute('data-impression-name')
-                       + "\n" + card.get_attribute('href') + "\n"
-                       + card.get_attribute('data-impression-dimension13') + '✔')
+                send_email("Na sklade" + '\n-------------------------------'
+                           + card.get_attribute('data-impression-name')
+                           + "\n" + card.get_attribute('href') + "\n"
+                           + card.get_attribute('data-impression-dimension13') + '✔',
+                           "Wake the fuck up Samurai, there's a GPU to scalp")
 
-        elif "Očakávame" in card.get_attribute('data-impression-dimension13'):
-            print(card.get_attribute('data-impression-name') + " -> "
-                  + card.get_attribute('data-impression-dimension13') + " ❔")
+            elif "Očakávame" in card.get_attribute('data-impression-dimension13'):
+                print(card.get_attribute('data-impression-name') + " -> "
+                      + card.get_attribute('data-impression-dimension13') + " ❔")
 
-        else:
-            print(card.get_attribute('data-impression-name') + " -> "
-                  + card.get_attribute('data-impression-dimension13') + " ❌")
+            else:
+                print(card.get_attribute('data-impression-name') + " -> "
+                      + card.get_attribute('data-impression-dimension13') + " ❌")
 
-    time.sleep(5)
-    driver.refresh()
+        time.sleep(5)
+        driver.refresh()
+
+
+if __name__ == '__main__':
+    main()
---
+++ cierne-diery-scraper.py
@@ -0,0 +1,51 @@
+import logging
+import time
+import datetime
+from random import randrange
+import selenium.webdriver
+from selenium.webdriver.chrome.options import Options
+from alza_gpu_scraper import send_email
+
+url = 'https://eshop.ciernediery.sk/'
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = selenium.webdriver.Chrome(options=chrome_options)
+driver.implicitly_wait(20)
+mail = ''
+
+
+def main():
+    try:
+        while True:
+            driver.get(url)
+            print(url)
+            xpath = '//*[@id="nm-shop-browse-wrap"]/ul/li[1]/div[1]/a/span'
+            style_to_check = 'background-image: url("https://d3i9l7sj72swdx.cloudfront.net/eshop-ciernediery-sk/Prelepka_VYPREDANE.png");'
+            time.sleep(randrange(5, 10, 1))
+
+            if driver.find_element_by_xpath(xpath).get_attribute("style") != style_to_check:
+                current_time = datetime.datetime.now()
+                # screenshot = driver.get_screenshot_as_png()
+                logging.info('Sending email at {}'.format(current_time))
+                send_email("{} at time: {}".format(url, current_time), "Update on {}".format(url), mail,
+                           '', '')
+                logging.info('Email sent at {}'.format(datetime.datetime.now()))
+                logging.info("sleeping for {}".format(str(300)))
+                time.sleep(300)
+            else:
+                timer = randrange(120, 300)
+                logging.info("sleeping for {}".format(str(timer)))
+                time.sleep(timer)
+                driver.refresh()
+    except Exception as exception:
+        current_time = datetime.datetime.now()
+        send_email("Exception thrown: \n{}".format(exception),
+                   "Cierne diery monitoring failed at time: {}".format(current_time), mail,
+                   '', '')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
